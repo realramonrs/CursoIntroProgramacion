afbe568788f24a760de6ed96b5d3e6cf65252a7a
RamonRS <celmon1@gmail.com>
2025-02-16 13:08:41 +0100

    Dia1

---
+++ JAVA/01 - Tipos de datos/A_ConceptosIniciales.java
@@ -0,0 +1,13 @@
+package tiposDeDatos;
+
+public class A_ConceptosIniciales {
+	//La clase es la unidad mínima lógica de encapsulación de código
+	//Las clases contienen métodos que es donde programamos nuestros algoritmos.
+	//Las clases se agrupan en packages
+	//Los packages se agrupan en módulos
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JAVA/01 - Tipos de datos/B_VariablesPrimitivas.java
@@ -0,0 +1,23 @@
+package tiposDeDatos;
+
+public class B_VariablesPrimitivas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Los datos se almacenan en variables. En función del valor que queramos guardar, y por lo tanto del espacio 
+		//que necesitamos en memoria , los lenguajes ponen a nuestra disposición diferentes tipos de variables.
+		// byte -->  Valores enteros que se codifican con 8 bits -128 a 127 (el menos ocupa 1 bit)
+		byte b1 = 1; 
+		// short --> Valores enteros que se codifican con 16 bits 
+		// int -->   Valores enteros que se codifican con 32 bits 
+		// long -->  Valores enteros que se codifican con 64 bits 
+		// float --> Valores decimales(hasta 8) que se codifican con 32 bits 
+		// double--> Valores decimales (doble precisión 16) que se codifican con 64 bits
+		// char --> Almacena caracter UNICODE (16 bits)
+		// boolean --> Almacena true o false (8 bits)
+		//En Java los tipos de variables se clasifican como
+		//Primitivos: Sólo almacenan un valor -> byte,short, int , long , float, double, char , boolean
+		//Complejos: Además de almacenar un valor , pueden realizar acciones (ejecutan métodos)
+	}
+
+}
