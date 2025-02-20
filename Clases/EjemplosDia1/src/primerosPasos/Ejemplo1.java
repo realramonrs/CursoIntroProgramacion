ae339a74a7870d8acb3b669f5d55a5c2503aa385
RamonRS <celmon1@gmail.com>
2025-02-20 17:25:47 +0100

    Clase Dia 1

---
+++ Clases/EjemplosDia1/src/primerosPasos/Ejemplo1.java
@@ -0,0 +1,31 @@
+package primerosPasos;
+
+public class Ejemplo1 {
+
+	public static void  pintarPiernas() {
+		System.out.println("    ||    ||");
+		System.out.println("   _||    ||_");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Me encanta esta clase de programacion");
+		System.out.println();
+		
+		System.out.println("Java es impresionante");
+		//Este código pinta la cara de un gato
+		System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======"); 
+        System.out.println("  (             )  ");
+        /*
+         * Esto es un comentario de varias
+         * lineas.
+         */
+       pintarPiernas();
+       System.out.println(" FIN DEL PROGRAMA");
+	}
+
+}
---
+++ Clases/EjemplosDia1/src/primerosPasos/Variables.java
@@ -0,0 +1,34 @@
+package primerosPasos;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Variables numéricas
+		int nota; //Variable sin inicializar
+		
+		int nota1 = 0,nota2 = 1,nota3 = 3;
+		int notaFinal = nota1 + nota2 + nota3;
+		
+		byte edad = 19; // Variable inicializada
+		short numero = 9000;
+		
+		byte n1 = 10, n2 = 9;
+		int suma = n1 + n2;
+		
+		long numeroGrande = 7_838_383_800l;
+		
+		float n4 = 9.56F;
+		double n5 = 6.78484848;
+		boolean condicion = true;
+		
+		char caracter = 'A';
+		String frase = "Esto es una pasada";
+		System.out.println(frase.charAt(0));
+		
+		
+		
+	}
+
+}
